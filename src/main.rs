add66a0fdaa4070b14d48423f50db48d6548aa9b
PassionMan1130 <123367712+ElvisJin@users.noreply.github.com>
2023-02-02 00:45:15 +0900

    add launch.json but not working

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
 #[derive(Debug)]
-
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square (size: u32) -> Self {
+        Self {
+            width: size,
+            height: size
+        }
+    }
 }
 
 fn main() {
@@ -13,4 +33,8 @@ fn main() {
     };
 
     dbg!(&rect1);
+
+    println!("area {}", rect1.area());
+    println!("width {}", rect1.width());
+    println!("square {:?}", Rectangle::square(3));
 }
